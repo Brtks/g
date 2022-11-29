27744831f0518b214b65fe0e245f3ccf992f0e20
Brtks <89167051+Brtks@users.noreply.github.com>
2022-11-29 19:58:54 -0300

    Update script.js

--- content/randomgame/script.js
+++ content/randomgame/script.js
@@ -6,18 +6,26 @@ var wr = document.getElementById('wr');
 var hintBtn = document.getElementById('hint-btn')
 var giveBtn = document.getElementById('give-btn')
 var hintTxt = document.getElementById('hint-txt');
-var tries = 11
+var tries = 10
 
+hintBtn.style.display = "none";
+giveBtn.style.display = "none";
 
 function done() {
 	if (parseFloat(input.value) != secretNum) {
 		wr.innerHTML = "Wrong!"
 		hintBtn.style.display = "inline-block";
-		giveBtn.style.display = "auto";
+		giveBtn.style.display = "inline-block";
 		hintTxt.innerHTML = "";
 		tries = tries - 1;
 	} else if (parseFloat(input.value) == secretNum) {
-		wr.innerHTML = "your luck: " + tries + "/10"
+		if (tries > 5 && tries < 10 || tries < 5 && tries > 0) {
+			wr.innerHTML = "your luck: " + tries + "/10"
+		} else if (tries == 10) {
+			wr.innerHTML = "you're very lucky today... congrats"
+		} else if (tries < 0) {
+			wr.innerHTML = "oh my god... how many mirrors have you broke?"
+		}
 		hintBtn.style.display = "none";
 		giveBtn.style.display = "none";
 		doneButton.style.display = "none";
@@ -33,12 +41,22 @@ function reload() {
 function hint() {
 	if (parseFloat(input.value) > secretNum) {
 		hintTxt.innerHTML = input.value + " is bigger than the secret number!"
+		tries = tries - 1
+		hintTxt.style.display = "block"
+		setTimeout(hide, 2500)
 	} else if (parseFloat(input.value) < secretNum) {
 		hintTxt.innerHTML = input.value + " is smaller than the secret number!"
+		tries = tries - 1
+		hintTxt.style.display = "block"
+		setTimeout(hide, 2500)
 	} else if (parseFloat(input.value) == secretNum) {
 		hintTxt.innerHTML = ""
-		tries = tries - 1
 	}
+}
+
+function hide() {
+	hintBtn.style.display = "none";
+	hintTxt.style.display = "none"
 }
 
 function give() {
